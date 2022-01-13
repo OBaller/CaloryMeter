0a8ce027b9da7015d877e4b1332a8bf3fb29e874
brightsummit.co <naseemoyebola@gmail.com>
2022-01-13 21:56:07 +0100

    initial Commit

---
+++ CaloryMeter/CaloryMeter/CaloryMeterApp.swift
@@ -0,0 +1,17 @@
+//
+//  CaloryMeterApp.swift
+//  CaloryMeter
+//
+//  Created by naseem on 13/01/2022.
+//
+
+import SwiftUI
+
+@main
+struct CaloryMeterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
