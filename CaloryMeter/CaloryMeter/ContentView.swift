55c24730417185a57dfd159afd990a80929761a8
brightsummit.co <naseemoyebola@gmail.com>
2022-01-15 14:21:25 +0100

    MenuView

--- CaloryMeter/CaloryMeter/ContentView.swift
+++ CaloryMeter/CaloryMeter/ContentView.swift
@@ -8,9 +8,10 @@
 import SwiftUI
 
 struct ContentView: View {
+    @StateObject var menu = Menu()
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        MenuView()
+            .environmentObject(menu)
     }
 }
 
---
+++ CaloryMeter/CaloryMeter/MenuView.swift
@@ -0,0 +1,51 @@
+//
+//  MenuView.swift
+//  CaloryMeter
+//
+//  Created by naseem on 15/01/2022.
+//
+
+import SwiftUI
+
+struct MenuView: View {
+    @EnvironmentObject var menu: Menu
+    
+    let columns = [GridItem(.adaptive(minimum: 150))]
+    
+    var body: some View {
+        NavigationView {
+            ScrollView {
+                LazyVGrid(columns: columns, pinnedViews: .sectionHeaders) {
+                    ForEach(menu.sections) { section in
+                        Section {
+                            ForEach(section.drinks) { drink in
+                                VStack {
+                                    Text(drink.name)
+                                        .font(.system(.body, design: .serif))
+                                    
+                                }
+                                .padding(.bottom)
+                            }
+                        } header: {
+                            Text(section.name)
+                                .font(.system(.title, design: .serif))
+                                .frame(maxWidth: .infinity, alignment: .leading)
+                                .padding([.top, .bottom, .trailing], 5)
+                                .background(.background)
+                        }
+                        
+                    }
+                }
+                .padding(.horizontal )
+            }
+            .navigationBarTitle("Add Drink")
+        }
+    }
+}
+
+struct MenuView_Previews: PreviewProvider {
+    static var previews: some View {
+        MenuView()
+            .environmentObject(Menu())
+    }
+}
